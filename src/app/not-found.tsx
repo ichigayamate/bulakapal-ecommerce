23d06968db7eec628c303cc15b3d957ec51981e0
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-02-19 23:20:37 +0700

    added redirect to homepage on notfound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,13 @@
 
 import {useRouter} from "next/navigation";
 import {useEffect} from "react";
+import toast from "react-hot-toast";
 
 export default function NotFound() {
   const router = useRouter();
 
   useEffect(() => {
+    toast.error("Page not found");
     router.push("/");
   })
 
