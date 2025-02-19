dadb9a0db900fed0d99fdee388d72dc0ee48aa81
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-02-19 09:23:20 +0700

    added pagination for get all products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,12 @@
 import ProductModel from "@models/product";
+import {Products} from "@interfaces/products";
+import {PaginatedData} from "@interfaces/api";
 
-export async function GET(): Promise<Response> {
-  const products = await ProductModel.getProducts();
+export async function GET(request: Request): Promise<Response> {
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page") ?? "1", 10);
+  const limit = parseInt(url.searchParams.get("limit") ?? "10", 10);
+
+  const products: PaginatedData<Products> = await ProductModel.getProducts(page, limit);
   return Response.json(products);
 }
