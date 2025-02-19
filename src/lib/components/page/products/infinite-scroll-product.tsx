c9c408981c8aa0a0605ce896fa4f17fce2468103
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-02-19 23:20:01 +0700

    added infinite scroll

---
+++ src/lib/components/page/products/infinite-scroll-product.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import {useState} from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import {Products} from "@interfaces/products";
+import {PaginatedData} from "@interfaces/api";
+import ProductCard from "@components/page/products/products-card";
+
+const Loader = () => <div className="flex justify-center mt-4"><span
+  className="loading loading-dots loading-md"></span></div>;
+
+export default function AllProducts({initialData}: Readonly<{ initialData: PaginatedData<Products> }>) {
+  const totalPage = Math.ceil(initialData.total / initialData.limit);
+  const [currentPage, setCurrentPage] = useState(initialData.page);
+  const [data, setData] = useState(initialData.data);
+  const [hasMore, setHasMore] = useState(initialData.page < totalPage);
+
+  const next = async () => {
+    const nextPage = currentPage + 1;
+    const url = new URL("http://localhost:3000/api/products");
+    url.searchParams.set("page", String(nextPage));
+    url.searchParams.set("limit", String(initialData.limit));
+
+    const response = await fetch(url.toString());
+    const nextData: PaginatedData<Products> = await response.json();
+
+    setData([...data, ...nextData.data]);
+    setCurrentPage(nextPage);
+    setHasMore(nextPage < totalPage);
+  }
+
+  return <InfiniteScroll
+    next={next}
+    hasMore={hasMore}
+    loader={<Loader/>}
+    dataLength={data.length}>
+    <div className="grid grid-cols-5 gap-4">
+      {data.map((product: Products) => (
+        <ProductCard data={product} key={String(product._id)}/>
+      ))}
+    </div>
+  </InfiniteScroll>
+}
