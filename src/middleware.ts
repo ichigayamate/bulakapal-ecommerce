29e4b6ce33229c499a63cf78671135bcedc22186
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-02-19 23:20:26 +0700

    added middleware in apis

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import {cookies} from "next/headers";
+import {verifyToken} from "@scripts/api/jwt";
+import {NextRequest, NextResponse} from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const cookie = await cookies();
+  const auth = cookie.get("authorization");
+  if (request.nextUrl.pathname.startsWith("/api/wishlists")) {
+    if (!auth) return Response.json({
+      message: "Unauthorized"
+    }, {status: 401});
+
+    const [type, token] = auth.value.split(" ");
+    if (!type || type !== "Bearer") return Response.json({
+      message: "Token type is not supported"
+    }, {status: 401});
+
+    const decoded = await verifyToken(token);
+
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set("x-user-id", decoded._id);
+    requestHeaders.set("x-user-email", decoded.email);
+
+    return NextResponse.next({
+      request: {
+        headers: requestHeaders
+      }
+    })
+  }
+}
+
+export const config = {
+  matcher: ["/api/wishlists/:path*", "/wishlist"]
+}
