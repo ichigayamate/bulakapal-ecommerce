4436508fd4d1d1ca724fc407e2f31c41bc38104c
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-02-20 16:43:26 +0700

    added wishlist page for authorized user

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import {cookies} from "next/headers";
 
 export default async function Page() {
-  return <>
+  const cookie = await cookies();
+  const auth = cookie.get("authorization");
+
+  if (!auth) return <>
     <h1 className="font-bold text-2xl">Wishlist</h1>
     <p>Looking for your wishlist? Sign in to see your favourite items</p>
     <Link href="/login" className="btn btn-sm mt-4">Login</Link>
+  </>
+
+  return <>
+    <h1 className="font-bold text-2xl">Wishlist</h1>
+    <p>Wishlist items</p>
   </>
 }
