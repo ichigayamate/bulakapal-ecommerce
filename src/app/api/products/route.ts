1a4996e3de1667be0649f32c57e4d69cafd63664
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-02-20 17:25:33 +0700

    added filter by search and tags in products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -6,7 +6,9 @@ export async function GET(request: Request): Promise<Response> {
   const url = new URL(request.url);
   const page = parseInt(url.searchParams.get("page") ?? "1", 10);
   const limit = parseInt(url.searchParams.get("limit") ?? "10", 10);
+  const search = url.searchParams.get("search") ?? "";
+  const tags = url.searchParams.get("tags") ?? "";
 
-  const products: PaginatedData<Products> = await ProductModel.getProducts(page, limit);
+  const products: PaginatedData<Products> = await ProductModel.getProducts(page, limit, search, tags);
   return Response.json(products);
 }
--- src/app/db/models/product.ts
+++ src/app/db/models/product.ts
@@ -1,6 +1,7 @@
 import database from "@/app/db/config";
 import {PaginatedData} from "@interfaces/api";
 import {Products} from "@interfaces/products";
+import {Filter} from "mongodb";
 
 /**
  * ProductModel class
@@ -10,11 +11,29 @@ export default class ProductModel {
 
   /**
    * Get all products
+   *
+   * @param page
+   * @param limit
+   * @param search
+   * @param tags
    */
-  static async getProducts(page: number = 1, limit: number = 10) {
+  static async getProducts(page: number = 1, limit: number = 10, search?: string, tags?: string) {
     const skip = (page - 1) * limit;
-    const data = await this.collection.find().skip(skip).limit(limit).toArray() as Products[];
-    const total = await this.collection.countDocuments();
+    const query: {
+      name?: Filter<string>;
+      tags?: Filter<string>;
+    } = {};
+
+    if (search) {
+      query.name = { $regex: search, $options: "i" };
+    }
+
+    if (tags) {
+      query.tags = { $in: tags.split(",") };
+    }
+
+    const data = await this.collection.find(query).skip(skip).limit(limit).toArray() as Products[];
+    const total = await this.collection.countDocuments(query);
 
     const paginatedData: PaginatedData<Products> = {
       data,
