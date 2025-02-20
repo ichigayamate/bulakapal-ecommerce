8ce9341dbd597d878bbaba95d420666bc03c2f8b
windraseptamadya-asrah <windra.asrah@outlook.com>
2025-02-20 16:42:01 +0700

    added logout logic

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,1 +1,8 @@
-// TODO: Logout
+"use server";
+
+import {cookies} from "next/headers";
+
+export const logout = async () => {
+  const cookieStore = await cookies();
+  cookieStore.delete("authorization");
+}
